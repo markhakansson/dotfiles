34ca1114c6fb047a8c4380a8362011183b1754fe
Mark Hakansson <mark.hakansson@live.se>
2019-12-25 20:08:45 +0100

    Add tmux and update fish and alacritty with neccessary bindings

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,15 @@
 # Cargo path
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 
+# Env 
+set fish_greeting
+
+# Start tmux
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
+
 # Aliases
 alias c='clear'
 alias l='ls'
