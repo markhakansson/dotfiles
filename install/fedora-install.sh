05c56e0610a78439cea3c4a916d6e3b71efa7a10
Mark Hakansson <mark.hakansson@live.se>
2020-01-21 14:24:23 +0100

    Add mouse support to tmux

--- install/fedora-install.sh
+++ install/fedora-install.sh
@@ -13,14 +13,18 @@ sudo rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/maste
 # Add repos
 sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf copr enable pschyska/alacritty -y
 
 # Install most needed applications
 echo "# Installing applications..."
 sudo dnf install -y \
 alacritty \
 cava \
+chsh \
 codium \
 curl \
+fd \
 fish \
 git \
 neovim \
@@ -29,13 +33,18 @@ stow \
 tmux \
 vim \
 
-if [$XDG_CURRENT_DESKTOP == "GNOME"]
-then
+if [$XDG_CURRENT_DESKTOP == "GNOME"]; then
 	sudo dnf install -y \
 	gnome-tweaks \
-	gnome-shell-extension \
-
+	gnome-shell-extension-unite \
+	gnome-shell-extension-no-topleft-hot-corner \
 fi
+
+echo "# Changing default shell to fish"
+chsh -s `which fish` 
+
+echo "# Installing oh-my-fish"
+curl -L https://get.oh-my.fish | fish
 
 echo "# Fetching Rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
