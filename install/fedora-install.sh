51fa72fbfc726bcd6436470135ee68ec10e7557e
Mark Hakansson <mark.hakansson@live.se>
2020-02-28 08:40:17 +0100

    Update install script

--- install/fedora-install.sh
+++ install/fedora-install.sh
@@ -15,6 +15,9 @@ sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-r
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf copr enable pschyska/alacritty -y
+# sudo dnf copr enable mhonek/sway 
+# sudo dnf copr enable alebastr/waybar 
+sudo dnf copr enable xiaofwan/sway-copr
 
 # Install most needed applications
 echo "# Installing applications..."
@@ -27,15 +30,20 @@ codium \
 curl \
 fd \
 fish \
+fzf \
 git \
 keepassxc \
 neovim \
 ripgrep \
 rxvt-unicode \
 stow \
+sway \
+swaylock \
+swayidle \
 syncthing \
 tmux \
 vim \
+waybar \
 
 if [$XDG_CURRENT_DESKTOP == "GNOME"]; then
 	sudo dnf install -y \
