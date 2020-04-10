418d87bccad9fcb356fb524f7b9ef4691492ad95
Mark Hakansson <mark.hakansson@live.se>
2020-04-10 10:18:49 +0200

    Start with in vi mode in fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,9 +2,11 @@
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
 set -U fish_user_paths $HOME/bin $fish_user_paths
 
-
 # Env 
 set fish_greeting
+
+# Start with vi bindings
+fish_vi_key_bindings
 
 # Start tmux
 if status is-interactive
