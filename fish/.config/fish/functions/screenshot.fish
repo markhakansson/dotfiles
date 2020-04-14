60b30ecfe2f0158d8aebcc037a154ec9bcd1d0cd
Mark Hakansson <mark.hakansson@live.se>
2020-04-14 15:21:56 +0200

    (fish) add notification to screenshot functions

--- fish/.config/fish/functions/screenshot.fish
+++ fish/.config/fish/functions/screenshot.fish
@@ -1,3 +1,4 @@
 function screenshot --description 'Takes a screenshot using grim. Only works on Wayland'
     grim ~/Pictures/Screenshots/(date +%Y-%m-%d_%H-%M-%S)_grim.png
+    notify-send --expire-time="5000" --urgency="LOW" "📷 Screenshot taken! File added to ~/Pictures/Screenshots/!"
 end
--- fish/.config/fish/functions/screenshot_slurp.fish
+++ fish/.config/fish/functions/screenshot_slurp.fish
@@ -1,3 +1,4 @@
 function screenshot_slurp --description 'Takes a screenshot of a selection using slurp and grim'
     grim -g (slurp) ~/Pictures/Screenshots/(date +%Y-%m-%d_%H-%M-%S)_slurp.png
+    notify-send --expire-time="5000" --urgency="LOW" "📷 Screenshot taken! File added to ~/Pictures/Screenshots/!"
 end
