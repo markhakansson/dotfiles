025187609ab318771e28f74f46cba7ef2f88c8c2
Mark Hakansson <mark.hakansson@live.se>
2020-04-19 09:44:13 +0200

    (fish) move abbreviations from config.fish to its own file

---
+++ fish/.config/fish/abbreviations.fish
@@ -0,0 +1,25 @@
+#
+# Abbreviations
+#
+
+## Program replacements
+abbr ls exa
+abbr l exa
+abbr la 'exa -a'
+abbr ll 'exa -l'
+abbr vim 'nvim'
+abbr grep 'ripgrep'
+abbr du 'dust'
+abbr code 'codium'
+
+## Aliases 
+abbr c 'clear'
+abbr ... 'cd ../..'
+abbr fdh 'fd -H'
+
+## Git
+abbr g 'git'
+abbr ga 'git add'
+abbr gcm 'git commit -m'
+abbr gs 'git status'
+abbr gd 'git diff'
