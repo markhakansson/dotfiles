74a7e4bdaf52a65fe77d42a30ca085b4c0df7228
Mark Hakansson <mark.hakansson@live.se>
2021-05-19 10:30:15 +0200

    (fish) update abbrv

--- fish/.config/fish/abbreviations.fish
+++ fish/.config/fish/abbreviations.fish
@@ -2,56 +2,67 @@
 # Abbreviations
 #
 
-set -l exa 'exa --group-directories-first'
-
 ## Program replacements
+##
+### ls
 abbr ls 'exa --group-directories-first'
 abbr l 'exa --group-directories-first'
 abbr la 'exa --group-directories-first -a'
+abbr lat 'exa --group-directories-first -aTL=3'
 abbr ll 'exa --group-directories-first -l'
+abbr llt 'exa --group-directories-first -lTL=3'
+### Zoxide (short 'z')
+abbr cd 'z'
+### Other
 abbr vim 'nvim'
 abbr grep 'rg'
 abbr du 'dust'
 abbr code 'codium'
 
 ## Interactive/Verbose
+#
 abbr rm 'rm -v'
 abbr rmrf 'rm -rfI'
 abbr cp 'cp -v'
 abbr mv 'mv -v'
 
 ## Aliases 
+#
 abbr c 'clear'
 abbr ... 'cd ../..'
 abbr .... 'cd ../../..'
 abbr fdh 'fd -H'
 abbr pg 'pgrep'
 
-# DNF
+## DNF
+#
 abbr dnfi 'sudo dnf install'
 abbr dnfu 'sudo dnf update'
 abbr dnfuy 'sudo dnf update -y'
 abbr dnfs 'dnf search'
 
-# Aura
+## Aura
+#
 abbr a 'sudo aura'
 abbr as 'sudo aura -S'
 abbr aa 'sudo aura -A'
 abbr asyu 'sudo aura -Syu'
 abbr aau 'sudo aura -Au'
 
-# Git
+## Git
+#
 abbr g 'git'
 abbr ga 'git add'
 abbr gcm 'git commit -m'
 abbr gs 'git status'
 abbr gd 'git diff'
 
-# Cargo
+## Cargo
+#
 abbr cua 'cargo-install-update install-update -a'
 
-# Systemctl
+## Systemctl
+#
 abbr sse 'sudo systemctl enable'
 abbr sss 'sudo systemctl start'
 abbr ss 'systemctl status'
-
